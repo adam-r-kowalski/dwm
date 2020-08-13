1884e1d24ae1ffccb243bb62415ab72b8c3a73f5
Adam Kowalski <adam.kowalski.work@gmail.com>
2020-08-13 13:27:31 -0700

    dwm colorscheme

--- config.def.h
+++ config.def.h
@@ -12,10 +12,14 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#8be9fd";
+
+static const char background[]      = "#1d2021";
+static const char foreground[]      = "#d4be98";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { foreground, background, col_gray2 },
+	[SchemeSel]  = { foreground, foreground, col_cyan  },
 };
 
 /* tagging */
