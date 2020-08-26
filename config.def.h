45142f0794c117f9da56d0ff00ae01309be85460
Adam Kowalski <adam.kowalski.work@gmail.com>
2020-08-26 16:37:10 -0700

    oceanic next

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -13,12 +13,12 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#8be9fd";
 
-static const char background[]      = "#272727";
-static const char foreground[]      = "#ebdbb2";
+static const char background[]      = "#1b2b34";
+static const char foreground[]      = "#ffffff";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { foreground, background, background },
+	[SchemeNorm] = { foreground, background, foreground },
 	[SchemeSel]  = { background, foreground, background  },
 };
 
